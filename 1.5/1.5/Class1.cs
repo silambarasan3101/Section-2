10041bb25d737f3e07b5a61378b1032636cda9bf
Silambarasan <msilambarasan98@gmail.com>
2023-09-18 17:51:59 +0530

    first commit

---
+++ 1.5/1.5/Class1.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._5
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void BasicAssertions()
+        {
+            int total = 100, marks1 = 60, marks2 = 75;
+            string name = null;
+
+            Assert.That(marks1, Is.Not.EqualTo(marks2));
+            Assert.That(marks1, Is.LessThan(marks2));
+            Assert.That(marks2, Is.InRange(50, 75));
+
+            Assert.That(name, Is.Null);
+        }
+    }
+}
---
+++ 1.6/1.6-/Class1.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._6_
+{
+    [TestFixture]
+    public class Class1
+    {
+
+        public void Warnings()
+        {
+            int total = 100, marks1 = 60, marks2 = 75;
+            string name = null;
+
+            Warn.If(marks1 > 100);
+            Warn.If(name == null);
+
+            Warn.Unless(marks1 + marks2 < 200);
+
+            Assert.Warn("This is a warning message");
+        }
+
+
+
+    }
+}
